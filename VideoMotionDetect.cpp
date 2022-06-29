3c1c29df06fc3de58bc748eaab20bfc8320bc590
Domenico Erriquez <domenico.erriquez@gmail.com>
2022-06-29 18:52:19 +0200

    Added pointers to functions

--- VideoMotionDetect.cpp
+++ VideoMotionDetect.cpp
@@ -10,12 +10,7 @@ using namespace ff;
 using namespace cv;
 using namespace std;
 
-
-
-
-
 class sharedQueue{
-
 
 };
 
@@ -35,30 +30,30 @@ int h3[3][3] = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
 int actualFrame = 0;
 vector<thread> listThreads;
 
-Mat calcGrey(Mat &image)
+Mat calcGrey(Mat *image)
 {
     Mat bgr[3];
-    split(image, bgr);
+    split(*image, bgr);
     Mat imgGray = (bgr[0] + bgr[1] + bgr[2]) / 3;
     return imgGray;
 }
 
-Mat calcSmooth(Mat &imgGrey)
+Mat calcSmooth(Mat *imgGrey)
 {
-    Mat imgSmooth = imgGrey.clone();
+    Mat imgSmooth = imgGrey->clone();
     // blur(imgGrey, imgSmooth, Size(3, 3));
-    for (int i = 0; i < imgGrey.rows; i++)
+    for (int i = 0; i < imgGrey->rows; i++)
     {
-        for (int j = 0; j < imgGrey.cols; j++)
+        for (int j = 0; j < imgGrey->cols; j++)
         {
             short count = 0;
             int value = 0;
-            for (int I = max(i - 1, 0); I < min(i + 2, imgGrey.rows); ++I)
+            for (int I = max(i - 1, 0); I < min(i + 2, imgGrey->rows); ++I)
             {
-                for (int J = max(j - 1, 0); J < min(j + 2, imgGrey.cols); ++J)
+                for (int J = max(j - 1, 0); J < min(j + 2, imgGrey->cols); ++J)
                 {
                     count++;
-                    value = value + (uchar)imgGrey.at<uchar>(I, J); // reason of uchar: CV_8U is unsigned 8bit/pixel - ie a pixel can have values 0-255, this is the normal range for most image and video formats.
+                    value = value + (uchar)imgGrey->at<uchar>(I, J); // reason of uchar: CV_8U is unsigned 8bit/pixel - ie a pixel can have values 0-255, this is the normal range for most image and video formats.
                 }
             }
             imgSmooth.at<uchar>(i, j) = value / count;
@@ -67,12 +62,12 @@ Mat calcSmooth(Mat &imgGrey)
     return imgSmooth;
 }
 
-int calcImg(Mat &image, Mat &firstFrame)
+int calcImg(Mat *image, Mat *firstFrame)
 {
     Mat diffImage;
-    image = calcGrey(image);
-    image = calcSmooth(image);
-    absdiff(firstFrame, image, diffImage);
+    *image = calcGrey(image);
+    *image = calcSmooth(image);
+    absdiff(*firstFrame, *image, diffImage);
     float numberDifferentPixels = countNonZero(diffImage); // PIXEL UGUALI A 0 SIGNIFICA CHE SONO UGUALI ALL'IMMAGINE COMPARATA
     float size = diffImage.rows * diffImage.cols;
     float result = numberDifferentPixels / size;
@@ -107,47 +102,47 @@ void takeImage()
         enter = true;
         l.unlock();
         if (enter && !image.empty())
-            localresult += calcImg(image, firstFrame);
+            localresult += calcImg(&image, &firstFrame);
     }
     differentFrames += localresult;
 }
 
-class emitter: public ff_monode_t <Mat>{
+// class emitter: public ff_monode_t <Mat>{
 
-public:
-    emitter(){}
+// public:
+//     emitter(){}
 
-    Mat *svc(Mat * image)
-    {
-        int contatore=0;
-        while (cap.isOpened() && !endVideo)
-        {
-            Mat image;
-            cap.read(image);
-            if (image.empty())
-            {
-                cout << "EMPTY" << endl;
-                endVideo = true;
-                return(EOS);
-            }
-            contatore++;
-            cout<< "CONTATORE: "<< contatore <<endl;
-            ff_send_out(&image);
-        }
-        return (EOS);
-    }
-};
+//     Mat *svc(Mat * image)
+//     {
+//         int contatore=0;
+//         while (cap.isOpened() && !endVideo)
+//         {
+//             Mat image;
+//             cap.read(image);
+//             if (image.empty())
+//             {
+//                 cout << "EMPTY" << endl;
+//                 endVideo = true;
+//                 return(EOS);
+//             }
+//             contatore++;
+//             cout<< "CONTATORE: "<< contatore <<endl;
+//             ff_send_out(&image);
+//         }
+//         return (EOS);
+//     }
+// };
 
-class calculate : public ff_node_t <Mat>{
+// class calculate : public ff_node_t <Mat>{
 
-public:
-    calculate(){}
-    Mat *svc(Mat * image){
-        cout << "GOT IT " <<endl;
-        // differentFrames+= calcImg(image,firstFrame);
-        return(GO_ON);
-   }
-};
+// public:
+//     calculate(){}
+//     Mat *svc(Mat * image){
+//         cout << "GOT IT " <<endl;
+//         // differentFrames+= calcImg(image,firstFrame);
+//         return(GO_ON);
+//    }
+// };
 
 int main(int argc, char **argv)
 {
@@ -174,8 +169,8 @@ int main(int argc, char **argv)
             if (actualFrame == 0)
             {
                 firstFrame = image.clone();
-                firstFrame = calcGrey(firstFrame);
-                firstFrame = calcSmooth(firstFrame);
+                firstFrame = calcGrey(&firstFrame);
+                firstFrame = calcSmooth(&firstFrame);
             }
             else
             {
@@ -199,19 +194,17 @@ int main(int argc, char **argv)
     {
         if (ff == 1) // COMPUTAZIONE CON FASTFLOW
         {
-            emitter s1;
-            calculate s2;
+            // emitter s1;
+            // calculate s2;
             Mat image;
             cap.read(image);
             firstFrame = image.clone();
-            firstFrame = calcGrey(firstFrame);
-            firstFrame = calcSmooth(firstFrame);
+            firstFrame = calcGrey(&firstFrame);
+            firstFrame = calcSmooth(&firstFrame);
 
-            ff_Pipe<> pipe(s1,s2);
-            pipe.run_and_wait_end();
-            cout<< "DifferentFrames: " << differentFrames <<endl;
-
-
+            // ff_Pipe<> pipe(s1,s2);
+            // pipe.run_and_wait_end();
+            // cout<< "DifferentFrames: " << differentFrames <<endl;
 
             // farm di fastflow
         }
@@ -229,10 +222,10 @@ int main(int argc, char **argv)
                 if (actualFrame == 0)
                 {
                     firstFrame = image.clone();
-                    firstFrame = calcGrey(firstFrame);
-                    firstFrame = calcSmooth(firstFrame);
+                    firstFrame = calcGrey(&firstFrame);
+                    firstFrame = calcSmooth(&firstFrame);
                 }
-                differentFrames += calcImg(image, firstFrame);
+                differentFrames += calcImg(&image, &firstFrame);
                 char key = waitKey(1);
                 actualFrame++;
                 if (key == 'q')
